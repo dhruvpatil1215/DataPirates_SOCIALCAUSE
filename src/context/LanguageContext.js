74d4e7d8ad49c5d4f7b69d56264d7a16023af2e4
dhruvpatil1215 <148572263+dhruvpatil1215@users.noreply.github.com>
2026-02-22 01:33:30 +0530

    Add files via upload

---
+++ src/context/LanguageContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useContext } from 'react';
+import { translations } from '../data/translations';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+    const [language, setLanguage] = useState('en');
+
+    const t = (key) => {
+        return translations[language][key] || translations['en'][key] || key;
+    };
+
+    return (
+        <LanguageContext.Provider value={{ language, setLanguage, t }}>
+            {children}
+        </LanguageContext.Provider>
+    );
+};
+
+export const useLanguage = () => {
+    const context = useContext(LanguageContext);
+    if (!context) {
+        throw new Error('useLanguage must be used within a LanguageProvider');
+    }
+    return context;
+};
